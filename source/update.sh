d7cd03c4797f98238757e617212dd500a8664647
Mingwei Xu <xumw1996@gmail.com>
2019-10-31 18:42:21 -0700

    add echo in sh

--- source/update.sh
+++ source/update.sh
@@ -1,2 +1,3 @@
 # copy files into car directory in order to get everything working
 cp manage_modified.py ~/projects/d3/
+echo "Please go to ~/projects/d3 and run command: python manage_modified.py drive"
