f9ec68952c3772881369ea779a32665e6826cfe0
InspireX96 <xumw1996@gmail.com>
2019-11-26 19:21:46 -0800

    fix yolo processor bug

--- donkeyacc/parts/yolo_processor.py
+++ donkeyacc/parts/yolo_processor.py
@@ -142,6 +142,9 @@ class YoloProcessor(object):
                        values are corresponding confidence (float, 0~1)
         """
         # TODO: multi thread and non blocking
+        if not image:
+            logging.warning('No input image to YOLO processor')
+            return self.result
         if not self.is_running:
             self.is_running = True
             self._save_image(image)  # save img
